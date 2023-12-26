043f603510dd86f4de3818bf6d9417681a358e24
AnnachiPSP <suryapalanivel@gmail.com>
2023-12-26 18:10:43 +0400

    temprary dashboard page created

--- src/pages/Student/Dashboard/index.tsx
+++ src/pages/Student/Dashboard/index.tsx
@@ -1,19 +1,58 @@
-import React from 'react'
-import StudentNavbar from '../../../components/StudentNavbar'
+import React from 'react';
+import StudentNavbar from '../../../components/StudentNavbar';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './index.css'
+import './index.css';
+import myImg from "../../../assets/images/Surya photo 3.5x3.5.jpg"
 
 function Dashboard() {
   return (
     <>
-        < StudentNavbar />
+      <StudentNavbar />
+      <div className='dashboard-body'>
         <div className='container'>
-            Dashboard
+          <main role='main' className='col-md-9 ml-sm-auto col-lg-10 px-md-4'>
+            <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
+              {/* 1. Welcome Message with Image */}
+              <div className='welcome-message'>
+                <img src={myImg} alt='Profile' className='profile-image' />
+                <h1 className='h2'>Welcome back, Surya Prakash</h1>
+              </div>
+            </div>
+
+            <div className='row'>
+              <div className='col-md-6'>
+                <div className='card mb-4 shadow-sm'>
+                  <div className='card-body'>
+                    {/* 2. Courses List */}
+                    <h5 className='card-title'>Courses</h5>
+                    <ul className='course-list'>
+                      <li>Algorithms Design</li>
+                      <li>Bioinformatics</li>
+                      <li>Introduction to DataBases</li>
+                      <li>Calculus IV</li>
+                      <li>Learning and Memory</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6'>
+                <div className='card mb-4 shadow-sm'>
+                  <div className='card-body'>
+                    {/* 3. Circular Progress */}
+                    <h5 className='card-title'>Credits</h5>
+                    <p className='card-text'>84% Progress Done</p>
+                    <div className='circular-progress'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
-        < ToastContainer />
+      </div>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/pages/Student/Profile/index.tsx
+++ src/pages/Student/Profile/index.tsx
@@ -8,20 +8,20 @@ function Profile() {
     <>
         < StudentNavbar />
         <div className='profile-body'>
-        <div className="profile-container">
-          <img className="profile-picture" src={profilePic} alt="Profile Picture" />
-          <div className="profile-details">
-            <h2>User Name</h2>
-            <p><strong>Gender:</strong> Male</p>
-            <p><strong>Father Name:</strong> John Doe Sr.</p>
-            <p><strong>DOB:</strong> January 1, 1990</p>
-            <p><strong>Branch:</strong> Computer Science</p>
-            <p><strong>Email:</strong> user@example.com</p>
-            <p><strong>Admission Date:</strong> January 1, 2022</p>
-            <p><strong>Phone Number:</strong> +1 (123) 456-7890</p>
-            <p><strong>Permanent Address:</strong> 123 Main St, City, Country</p>
+          <div className="profile-container">
+            <img className="profile-picture" src={profilePic} alt="Profile Picture" />
+            <div className="profile-details">
+              <h2>User Name</h2>
+              <p><strong>Gender:</strong> Male</p>
+              <p><strong>Father Name:</strong> John Doe Sr.</p>
+              <p><strong>DOB:</strong> January 1, 1990</p>
+              <p><strong>Branch:</strong> Computer Science</p>
+              <p><strong>Email:</strong> user@example.com</p>
+              <p><strong>Admission Date:</strong> January 1, 2022</p>
+              <p><strong>Phone Number:</strong> +1 (123) 456-7890</p>
+              <p><strong>Permanent Address:</strong> 123 Main St, City, Country</p>
+            </div>
           </div>
-        </div>
         </div>
     </>
   )
